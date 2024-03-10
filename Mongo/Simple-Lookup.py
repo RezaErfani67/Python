8174c499ba99081814644fac4d4d95cef00ef3f6
RezaErfani67 <RezaErfani67@gmail.com>
2024-03-10 22:23:54 +0330

    Update Simple-Lookup.py

--- Mongo/Simple-Lookup.py
+++ Mongo/Simple-Lookup.py
@@ -1,3 +1,45 @@
+#get from parchildest collection to parent
+# db.getCollection("address").aggregate(
+#     {
+#         "$lookup": {
+# 			"localField": 'user',
+# 			"from": 'user',
+# 			"foreignField": '_id',
+# 			"pipeline": [
+# 				{
+# 					"$lookup": {
+# 						"localField": 'group',
+# 						"from": 'group',
+# 						"foreignField": '_id',
+# 						as: 'groups',
+# 					},
+# 				},
+# 			],
+# 			as: 'users',
+# 	    	}
+# 		})
+
+#get from parent to childest
+# db.getCollection("group").aggregate(
+#     {
+#         "$lookup": {
+# 			"localField": '_id',
+# 			"from": 'user',
+# 			"foreignField": 'group',
+# 			"pipeline": [
+# 				{
+# 					"$lookup": {
+# 						"localField": '_id',
+# 						"from": 'address',
+# 						"foreignField": 'user',
+# 						as: 'address',
+# 					},
+# 				},
+# 			],
+# 			as: 'users',
+# 	    	}
+# 		})
+
 def convert_to_lookup(relationship_map):
     lookup_array = []
     for key, value in relationship_map.items():
@@ -9,13 +51,10 @@ def convert_to_lookup(relationship_map):
                 'as': key
             }
         }
-
         if 'nested' in value:
             nested_lookup = convert_to_lookup(value['nested'])
             lookup['$lookup']['pipeline'] = nested_lookup
-
         lookup_array.append(lookup)
-
     return lookup_array
 
 # Test data
