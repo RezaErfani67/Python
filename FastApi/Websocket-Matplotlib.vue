68c2e8179b0cc48e5fe58de9742661b78cd25061
RezaErfani67 <RezaErfani67@gmail.com>
2024-03-21 10:09:00 +0330

    Create Websocket-Matplotlib.vue

---
+++ FastApi/Websocket-Matplotlib.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Matplotlib Plot</h1>
+    <img :src="plotUrl" alt="Matplotlib Plot">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      plotUrl: '',
+    };
+  },
+  mounted() {
+    this.setupWebSocket();
+  },
+  methods: {
+    setupWebSocket() {
+      const socket = new WebSocket('ws://localhost:8000/ws');
+      socket.onmessage = (event) => {
+        this.plotUrl = `data:image/png;base64,${event.data}`;
+      };
+    },
+  },
+};
+</script>
